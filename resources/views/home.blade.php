ef6793fe8c81ed05a3594e9d5e3aede85f39e39d
rudiansyah <rudiansyahcrypto@gmail.com>
2019-09-25 14:18:51 +0700

    First Commit, Starting Design the view

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://instagram.fbpn2-1.fna.fbcdn.net/vp/cbbfa2159a861f5783b36acb189493e1/5E189C84/t51.2885-19/s150x150/45399787_2165809970324365_3963366202654326784_n.jpg?_nc_ht=instagram.fbpn2-1.fna.fbcdn.net&_nc_cat=1" class="rounded-circle" alt="">
+        </div>
+        <div class="col-9 pt-5">
+            <div><h1>laraGram</h1></div>
+            <div class="d-flex">
+                <div><strong>223</strong> posts</div>
+                <div><strong>23k</strong> followers</div>
+                <div><strong>500</strong> following</div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
