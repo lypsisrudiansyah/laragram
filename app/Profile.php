e0e494f78f4e0be0b7cb46b918c1c81c01b1fbae
lypsisrudiansyah <rudiansyahether@gmail.com>
2019-09-26 21:13:51 +0700

    Add Accessor,Add Follow Method etc.
    [ADD] an Accessor For Image
    
    [ADD] Following People
    
    [ADD] Simple Front-End Processing with vue.js

--- app/Profile.php
+++ app/Profile.php
@@ -9,6 +9,17 @@ class Profile extends Model
 {
 	protected $guarded = [];
 
+	public function profileImage()
+	{
+		$imagePath = ($this->image) ? $this->image : 'profile/cHea3RmB5RCuxuLaMtLLygkuFFwiCLEVHFAT6R4C.png';
+		return '/storage/' . $imagePath;
+	}
+
+    public function followers()
+    {
+    	return $this->belongsToMany(User::class);
+    }
+
     public function user()
     {
     	return $this->belongsTo(User::class);
